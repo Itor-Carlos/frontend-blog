9ae548541fcf5821e7c5f198692259c9e6ad48b4
Itor Carlos <itor51carlos@gmail.com>
2024-05-27 14:31:17 -0300

    reset css

--- src/app/components/Global/globalStyle.ts
+++ src/app/components/Global/globalStyle.ts
@@ -1,11 +1,13 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components"
 
-export const GlobalStyle = styled.body`
-    font-family: Arial, sans-serif;
-    background-color: #f9f9f9;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-family: Arial, sans-serif;
+        background-color: #f9f9f9;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 `
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "./App.css"
 import { GlobalStyle } from './app/components/Global/globalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle>
-        <App />
-    </GlobalStyle>
+    <GlobalStyle/>
+    <App/>
   </React.StrictMode>,
 )
