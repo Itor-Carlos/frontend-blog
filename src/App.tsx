1a0c0d4e7dcbaede8442198dfda5e30506509521
Itor Carlos <itor51carlos@gmail.com>
2024-05-29 17:04:46 -0300

    alterado localizacao da pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Home } from "./app/pages/Home"
+import { Home } from "./app/pages/Home/Home"
 
 function App() {
   return (
--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
-import { Header } from "../components/Header";
-import { Post } from "../components/Post";
+import { Header } from "../../components/Header";
+import { Post } from "../../components/Post";
 
 export function Home(){
     return (
