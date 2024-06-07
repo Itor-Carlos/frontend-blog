c9dec766264570fe75335749f2afc05aaa527ea5
Itor Carlos <itor51carlos@gmail.com>
2024-06-07 17:24:09 -0300

    fix: alterado para label ser qualquer elemento React valido

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 interface ButtonProps{
     onClick?: () => void;
-    label: string;
+    label: React.ReactNode;
     disabled?: boolean;
     link?: string;
 }
 
 export function Button({onClick, label, disabled=false, link = "#"}: ButtonProps){
+
     return (
         <Link to={link}>
             <button  disabled={disabled} onClick={onClick}>{label}</button>
