9e3c81b8758ac73d834399f637235dec9c60fea0
Itor Carlos <itor51carlos@gmail.com>
2024-06-24 19:37:10 -0300

    removido : da interface de login

--- src/app/pages/Login/index.tsx
+++ src/app/pages/Login/index.tsx
@@ -6,9 +6,9 @@ export function LoginPage(){
         <LoginContainer>
             <h1>Login</h1>
             <form>
-                <label>Email:</label>
+                <label>Email</label>
                 <input type="email" id="email" name="email" required />
-                <label>Senha:</label>
+                <label>Senha</label>
                 <input type="password" id="password" name="password" required />
                 <ButtonContainer>
                     <Button label="Login"/>
