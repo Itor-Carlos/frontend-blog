888fc4e8ff5e85c919def6573572c49419243dc4
Itor Carlos <itor51carlos@gmail.com>
2024-07-06 14:15:00 -0300

    Notificacoes na página de login

--- src/app/pages/CadastroUser/index.tsx
+++ src/app/pages/CadastroUser/index.tsx
@@ -2,6 +2,9 @@
 import * as Yup from 'yup';
 import axios from "axios";
 import { FormComponent } from "../../components/Form";
+import { useNotificationBanner } from '../../hooks/useNotificationBanner';
+import { NotificationBanner } from '../../components/NotificationBanner';
+import { redirect } from 'react-router-dom';
 
 interface UserFormValues {
     nome: string;
@@ -29,7 +32,7 @@ const fields = [
 
 
 export function CadastroUser(){
-
+    const { visible, message, type, showNotification } = useNotificationBanner({ duration: 3000 });
     const initialValues: UserFormValues = {
         nome: "",
         senha: "",
@@ -49,21 +52,25 @@ export function CadastroUser(){
     })
 
     const handleSubmit = (values: UserFormValues) => {
-        console.log(values)
         axios.post("http://localhost:3000/register", values)
         .then((response) => {
-            console.log(response)
+            if(response.status === 200) showNotification("New user registered successfully!", 'success')
         })
-        .catch((response) => console.log(response))
+        .catch((response) => {
+            if(response.response.status === 400) showNotification(response.response.data.error, 'error')
+        })
     }
 
     return (
-        <FormComponent 
-            title="Cadastrar Usuario" 
-            fields={fields}
-            handleSubmit={handleSubmit}
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-        />
+        <>
+            <FormComponent 
+                title="Cadastrar Usuario" 
+                fields={fields}
+                handleSubmit={handleSubmit}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+            />
+            <NotificationBanner type={type} message={message} visible={visible}/>
+        </>
     )
 }
